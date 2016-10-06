f250264ea6fbf0339d83147958db90ea25d0fca1
guankaer <cool9932@sohu.com>
2016-10-06 15:57:39 +0800

    anychat 20161006 thought of login so delete the anychat code in mainactivity

--- app/src/main/java/guankaer/homecare/MainActivity.java
+++ app/src/main/java/guankaer/homecare/MainActivity.java
@@ -6,16 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-
 import guankaer.bluetooth.Bluetooth;
-import guankaer.anychat.*;
 
 
-public class MainActivity extends AppCompatActivity implements AnyChatBaseEvent{
+public class MainActivity extends AppCompatActivity{
 
     private ImageView Monitor, Records, Connect, Settings;
-    public AnyChatCoreSDK anyChatSDK;
-    private final int LOCALVIDEOAUTOROTATION = 1; // 本地视频自动旋转控制
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,41 +57,6 @@ public class MainActivity extends AppCompatActivity implements AnyChatBaseEvent{
                 }
             }
         });
-    }
-
-    private void InitSDK() {
-        if (anyChatSDK == null) {
-            anyChatSDK = AnyChatCoreSDK.getInstance(this);
-            anyChatSDK.SetBaseEvent(this);
-            anyChatSDK.InitSDK(android.os.Build.VERSION.SDK_INT, 0);
-            AnyChatCoreSDK.SetSDKOptionInt(
-                    AnyChatDefine.BRAC_SO_LOCALVIDEO_AUTOROTATION,LOCALVIDEOAUTOROTATION);
-        }
-    }
-
-    // 连接服务器消息, bSuccess表示是否连接成功
-    public void OnAnyChatConnectMessage(boolean bSuccess){
-
-    }
-    // 用户登录消息，dwUserId表示自己的用户ID号，dwErrorCode表示登录结果：0 成功，否则为出错代码
-    public void OnAnyChatLoginMessage(int dwUserId, int dwErrorCode){
-
-    }
-    // 用户进入房间消息，dwRoomId表示所进入房间的ID号，dwErrorCode表示是否进入房间：0成功进入，否则为出错代码
-    public void OnAnyChatEnterRoomMessage(int dwRoomId, int dwErrorCode){
-
-    }
-    // 房间在线用户消息，进入房间后触发一次，dwUserNum表示在线用户数（包含自己），dwRoomId表示房间ID
-    public void OnAnyChatOnlineUserMessage(int dwUserNum, int dwRoomId){
-
-    }
-    // 用户进入/退出房间消息，dwUserId表示用户ID号，bEnter表示该用户是进入（TRUE）或离开（FALSE）房间
-    public void OnAnyChatUserAtRoomMessage(int dwUserId, boolean bEnter){
-
-    }
-    //网络断开消息，该消息只有在客户端连接服务器成功之后，网络异常中断之时触发，dwErrorCode表示连接断开的原因
-    public void OnAnyChatLinkCloseMessage(int dwErrorCode){
-
     }
 
 }
